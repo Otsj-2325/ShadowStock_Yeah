4b59d1e703c844e50b637df705e2cffe79bb793b
IkarashiFumiya <116151695+IkarashiFumiya@users.noreply.github.com>
2023-11-03 15:26:51 +0900

    ライトエフェクト、サウンド追加

--- Assets/IF/cs/SCR_LightFall.cs
+++ Assets/IF/cs/SCR_LightFall.cs
@@ -6,18 +6,63 @@ using UnityEngine;
 
 public class SCR_LightFall : MonoBehaviour
 {
-    private Rigidbody cp_Rigidbody;
+    public float m_KickForce = 1.0f;
+
+
+    public Rigidbody cp_Rigidbody;
+    public bool m_IsKick;
+    public int m_IsKickCount;
+    [SerializeField] private GameObject m_PlayerObj;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_IsKick = false;
+        m_IsKickCount = 0;
+        // Ç±Ç±
+        GameObject childObject = gameObject.transform.GetChild(0).gameObject;
+        SCR_IsKick kickObject = childObject.GetComponent<SCR_IsKick>();
+        if (kickObject != null)
+        {
+            kickObject.SetIsKick(IsKick);
+        }
+
         cp_Rigidbody = GetComponent<Rigidbody>();
         cp_Rigidbody.useGravity = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (m_IsKick == true)
+        {
+            
+            m_IsKickCount++;
+            if (m_IsKickCount > 60)
+            {
+                m_IsKick = false;
+                m_IsKickCount = 0;
+            }
+        }
+        else
+        {
+            // å≈íË
+            //cp_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+
+    }
+
+    private void IsKick()
+    {
+        m_IsKick = true;
+
+        Vector3 direction = transform.position - m_PlayerObj.transform.position;
+        direction.Normalize();
+
+        cp_Rigidbody.AddForce(direction * m_KickForce, ForceMode.Impulse);
+        cp_Rigidbody.AddForce(Vector3.up * m_KickForce * 2, ForceMode.Impulse);
+
+        SCR_EffectManager.instance.EFF_Light(transform.position, transform.rotation);
+        SCR_SoundManager.instance.PlaySE(SE_Type.Gimmick_Light);//SEçƒê∂
     }
 }
